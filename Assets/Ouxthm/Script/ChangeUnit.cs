3b7b4c81f15d066a89bff84dd1c8a2e6abf75bc3
OuxtHm <blue29856@naver.com>
2024-03-26 00:57:29 +0900

    Excluding the unit you are using
    사용 중인 유닛을 제외할 수있도록 수정

--- Assets/Ouxthm/Script/ChangeUnit.cs
+++ Assets/Ouxthm/Script/ChangeUnit.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class ChangeUnit : MonoBehaviour
 {
+    public GameObject invisible;        // 빈 슬롯
+    public Button exceptionBtn;     // 사용 중인 유닛 제거 버튼
     public Transform useArray;      // 사용 중인 유닛 모음
     public Transform readyArray;        // 대기 중인 유닛 모음 
     public List<Button> readyBtns = new List<Button>();
@@ -20,9 +22,29 @@ public class ChangeUnit : MonoBehaviour
     public Transform parentSeconds;
     private void Awake()
     {
+        invisible = Resources.Load<GameObject>("UIPrefabs/InvisibleSlot");
+        exceptionBtn = transform.GetChild(2).GetComponent<Button>();
         useArray = transform.GetChild(0).GetComponent<Transform>();
         readyArray = transform.GetChild(1).GetComponent<Transform>();
         UpdateUnitList();
+        exceptionBtn.onClick.AddListener(() =>
+        {
+            selectFirst.transform.SetParent(readyArray);
+            selectFirst.transform.SetSiblingIndex(readyArray.childCount - 1);
+
+            GameObject invisibleSlot = Instantiate(invisible, useArray.transform);
+            invisibleSlot.transform.SetSiblingIndex(indexFirst);
+            selectFirst = null;
+            selectSeconds = null;
+            indexFirst = -1;
+            indexSeconds = -1;
+            parentFirst = null;
+            parentSeconds = null;   
+            readyBtns.Clear();
+            useBtns.Clear();
+            UpdateUnitList();
+            exceptionBtn.gameObject.SetActive(false);
+        });
     }
 
     void UpdateUnitList()       // 유닛 리스트 업데이트
@@ -31,6 +53,7 @@ public class ChangeUnit : MonoBehaviour
         {
             Button btn = readyArray.GetChild(i).GetComponent<Button>();
             readyBtns.Add(btn);
+            btn.onClick.RemoveAllListeners();
             int index = i; 
             btn.onClick.AddListener(() =>
             {
@@ -61,6 +84,7 @@ public class ChangeUnit : MonoBehaviour
         {
             Button btn = useArray.GetChild(i).GetComponent<Button>();
             useBtns.Add(btn);
+            btn.onClick.RemoveAllListeners();
             int index = i;
             btn.onClick.AddListener(() =>
             {
@@ -69,6 +93,10 @@ public class ChangeUnit : MonoBehaviour
                     selectFirst = useBtns[index].gameObject;
                     indexFirst = selectFirst.transform.GetSiblingIndex();
                     parentFirst = selectFirst.transform.parent;
+                    if (parentFirst.name == "UseArranger")
+                    {
+                        exceptionBtn.gameObject.SetActive(true);
+                    }
                 }
                 else if (selectSeconds == null)
                 {
@@ -87,6 +115,8 @@ public class ChangeUnit : MonoBehaviour
                 }
             });
         }
+        
+
     }
 
     void SwapUnit()     // 유닛 위치 변경
@@ -99,5 +129,6 @@ public class ChangeUnit : MonoBehaviour
 
         selectFirst = null;
         selectSeconds = null;
+        exceptionBtn.gameObject.SetActive(true);
     }
 }
